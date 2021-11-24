0f98700dce205f0209be49172061d9e4d8a503d7
hansenwuwu <eric248655665@gmail.com>
2021-11-24 18:59:57 +0800

    Add SayHello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hansenwuwu/go-module-example
+
+go 1.13
---
+++ say/say.go
@@ -0,0 +1,7 @@
+package say
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Say Hello from github.com/hansenwuwu/go-module-example")
+}
